c5912d24e01bcc692317283c272a6534bcd867ac
nadziroar <nadzirahzaid144@gmail.com>
2022-01-24 10:44:24 +0800

    Update the snake.py

--- snake.py
+++ snake.py
@@ -2,27 +2,36 @@
 # 2. Create the screen
 from curses import KEY_DOWN
 import pygame
+import time
 
 pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0 , 0)
- 
-dis= pygame.display.set_mode((800,600))
+
+#This is to initialize the display of the game
+display_width  = 800
+display_height = 600
+dis = pygame.display.set_mode((display_width, display_width))
 pygame.display.set_caption('Snake Game')
 
 game_over = False
 
 #To initialize the position of snake
-x1 = 300
-y1 = 300
+x1 = display_width/2
+y1 = display_height/2
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+snake_speed = 30
 
+font_style = pygame.font.SysFont(None, 50)
+def message(msg, color):
+    mesg = font_style.render(msg, True,  color)
+    dis.blit(mesg , [display_width/2, display_height/2])
 #To ensure the window is not closed until the game over
 while not game_over:
     for event in pygame.event.get():
