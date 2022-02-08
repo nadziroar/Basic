02a4c4bf4e7369c4e79ffac5d530b9420227cbda
nadziroar <nadzirahzaid144@gmail.com>
2022-02-08 16:11:42 +0800

    Create web_scraping.py

---
+++ web_scraping.py
@@ -0,0 +1,5 @@
+#This file is to demo how to do web scraping
+# 1. Go to the website you want to inspect
+# 2. Browser Tools > Web Developer Tools >  Inspect
+# 3. Find the data you want to inspect
+# 4. 
