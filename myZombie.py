b489348c64a6920830b4c5c82049c11aad5371b3
nadziroar <nadzirahzaid144@gmail.com>
2022-02-27 11:51:45 +0800

    Created Zombie class

--- myZombie.py
+++ myZombie.py
@@ -4,3 +4,15 @@ class myZombie :
     def __init__(self , name):
         #All zombies must have names 
         self.name  = name
+        
+    def turn(self , gameState):
+        #gameState is a dictionary with info about the current state of the game
+        #You can choose to ignore it in your code
+        
+        diceRollResults = zombiedice.roll() #First Role
+        #roll() returns a dictionary with keys 'brains' , 'shortgun' ,  and
+        #'footsteps' with how many rolls of each type there were
+        #The 'rolls' key is a list of (color , icon) tuples with the 
+        # exact roll result information.
+        # Examples of a roll() return value:
+        
