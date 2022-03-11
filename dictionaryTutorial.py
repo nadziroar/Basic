34c300e97ec49d5c3f9e09e043f791a1b23033da
nadziroar <nadzirahzaid144@gmail.com>
2022-03-11 21:08:15 +0800

    Update on dictionary.py

--- dictionaryTutorial.py
+++ dictionaryTutorial.py
@@ -11,3 +11,23 @@ for name , todolists in people.items():
     print(f"\n{name.title()}'s  to do list are : ")
     for todolist in todolists:
         print(f"\t{todolist.title()}") 
+        
+#Add value to Dictionary
+class DictionarySample:
+    def __init__(self) :
+        self = dict()
+        
+    #Function to add item to DictionarySample 
+    def add(self , key , value):
+        self[key] = value
+        
+dictionary1 = DictionarySample()
+
+dictionary1.key = input('Enter the key : ')
+dictionary1.value = input ('Enter the value : ')
+
+dictionary1.add(dictionary1.key, dictionary1.value)
+dictionary1.add(2 , 'Student 2')
+
+print(dictionary1)
+        
